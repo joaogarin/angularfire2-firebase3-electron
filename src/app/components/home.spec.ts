e9f04ffd632067b199171933c7c357741b9fd058
Joao Garin <joao.garin@gmail.com>
2016-06-23 11:33:08 +0200

    added test providers

--- src/app/components/home.spec.ts
+++ src/app/components/home.spec.ts
@@ -15,7 +15,26 @@ import {
 import { Component, provide } from '@angular/core';
 import {HomeComponent} from './home';
 
+
+// Firebase providers
+import {FIREBASE_PROVIDERS, defaultFirebase} from 'angularfire2';
+
+/**
+ * Basic configuration like Endpoint URL's, API version..
+ */
+const options = require('./../config.json');
+
 describe('App component', () => {
+
+    //setup
+    beforeEachProviders(() => [
+        defaultFirebase({
+            apiKey: options.firebase.apiKey,
+            authDomain: options.firebase.authDomain,
+            databaseURL: options.firebase.databaseURL,
+            storageBucket: options.firebase.storageBucket,
+        })
+    ]);
 
     @Component({
         template: ``,
@@ -23,12 +42,12 @@ describe('App component', () => {
     })
     class HomeTest {}
 
-    it('shoud render with h1 empty', async(inject([TestComponentBuilder], (tcb) => {
+    it('shoud render with h1 Firebase', async(inject([TestComponentBuilder], (tcb) => {
         tcb.overrideTemplate(HomeTest, '<ae-home></ae-home>')
             .createAsync(HomeTest).then((fixture: ComponentFixture<HomeTest>) => {
                 fixture.detectChanges();
                 let compiled = fixture.debugElement.nativeElement;
-                expect(compiled.querySelector('h1')).toHaveText('');
+                expect(compiled.querySelector('h1')).toHaveText('Firebase');
             });
     })));
 });
